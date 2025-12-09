077ea8b6afbedefd5b47b0247ce86454714c8236
yang <281651384@qq.com>
2025-12-09 23:00:29 +0800

    data

--- vite.config.js
+++ vite.config.js
@@ -67,12 +67,16 @@ export default defineConfig({
     },
     // 分块策略
     rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.message.includes('Comments in CSS')) return
+        warn(warning)
+      },
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString()
           }
-        }
+        },
       }
     }
   },
