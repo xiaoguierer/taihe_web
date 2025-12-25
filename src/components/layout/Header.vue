bd8d54fdab803638d78d5811183b2abb00c49550
xiaoguierer <pkali@sohu.com>
2025-12-26 07:27:53 +0800

    seo

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,6 @@
 <template>
   <header class="header">
     <meta name="p:domain_verify" content="f66800ca05e0fd20933922f7f8352c3e"/>
-    <meta name="p:domain_verify" content=""/>
     <!-- 品牌LOGO -->
     <div class="brand" @click="goToHome">
       <span class="brand-text">ZENIUL</span>
